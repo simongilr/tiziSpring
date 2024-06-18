4b6146754ceaf6e714d045aac4b858c3aaf968d3
Simon Jose Gil Romero <simon.gil@ada.co>
2024-06-18 14:12:53 -0500

    primer commit

---
+++ src/main/java/com/platzi/market/PlatiziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatiziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatiziMarketApplication.class, args);
+	}
+
+}
